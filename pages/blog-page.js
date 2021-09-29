09fafc7938b3db93308ea9e6c115120c2c30d05f
atsuya23 <90808047+atsuya23@users.noreply.github.com>
2021-09-29 21:21:20 +0900

    事前HTMLの作成

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,11 +1,23 @@
-import Layout from "../components/Layout"
+import Layout from "../components/Layout";
+import Post from "../components/Post";
 
-const Blog = () => {
-    return (
-        <Layout title="Blog">
-            Blog Page
-        </Layout>
-    )
+import { getAllPostData } from "../lib/post";
+
+const Blog = ({posts}) => {
+  return (
+    <Layout title="Blog">
+      <ul className="m-10">
+        {posts && posts.map((post) => <Post key={post.id} post={post} />)}
+      </ul>
+    </Layout>
+  );
+};
+
+export default Blog;
+
+export async function getStaticProps() {
+  const posts = await getAllPostData();
+  return {
+    props: { posts },
+  };
 }
-
-export default Blog
